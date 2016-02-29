4e4fb79263dd1f59991ade2c55d60708176fd93a
till straumann <till.straumann@alumni.tu-berlin.de>
2016-02-29 15:48:43 -0800

    - fixed botched merge conflict (result of rebase)

--- AxiMicronN25Q.h
+++ AxiMicronN25Q.h
@@ -1,31 +1,3 @@
-#ifndef _AXIMICRONN25Q_H
-#define _AXIMICRONN25Q_H
-
-#include <cpsw_api_builder.h>
-#include <cpsw_mmio_dev.h>
-
-//AxiMicronN25Q
-class   IAxiMicronN25QB;
-typedef shared_ptr<IAxiMicronN25QB> AxiMicronN25QB;
-
-
-class IAxiMicronN25QB : public virtual IMMIODev {
-public:
-    static AxiMicronN25QB create(const char *name);
-};
-
-class IAxiMicronN25Q;
-typedef shared_ptr<IAxiMicronN25Q> AxiMicronN25Q;
-
-class IAxiMicronN25Q : public virtual IEntry {
-public:
-    static AxiMicronN25Q create(Path p);
-public:
-};
-
-#endif
-
-=======
 //-----------------------------------------------------------------------------
 // File          : AxiMicronN25Q.h
 // Author        : Larry Ruckman  <ruckman@slac.stanford.edu>
