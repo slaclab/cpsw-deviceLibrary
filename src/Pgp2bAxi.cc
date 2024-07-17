7b296106bbbfd115ed7f3f377ec3e8f0e2497e32
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-07-17 16:30:01 -0700

    Apply license header to all source files

--- src/Pgp2bAxi.cc
+++ src/Pgp2bAxi.cc
@@ -1,3 +1,12 @@
+//////////////////////////////////////////////////////////////////////////////
+// This file is part of 'deviceLibrary'.
+// It is subject to the license terms in the LICENSE.txt file found in the 
+// top-level directory of this distribution and at: 
+//    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+// No part of 'deviceLibrary', including this file, 
+// may be copied, modified, propagated, or distributed except according to 
+// the terms contained in the LICENSE.txt file.
+//////////////////////////////////////////////////////////////////////////////
 #include "Pgp2bAxi.h"
 #include <cpsw_sval.h>
 #include <cpsw_entry_adapt.h>
